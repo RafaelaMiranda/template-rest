3d26efede7c589093bd135b99de4a535eee9da0d
Rafaela Miranda <rafaela-gomes@outlook.com>
2019-01-20 13:28:17 -0200

    Aula 4.3

---
+++ footer.php
@@ -0,0 +1,15 @@
+        <footer>
+        	<nav class="nav-footer">
+        		<ul>
+					<li class="current_page_item"><a href="/rest/">Menu</a></li>
+					<li><a href="/rest/sobre/">Sobre</a></li>
+					<li><a href="/rest/contato/">Contato</a></li>
+        		</ul>
+        	</nav>
+
+        	<p>Rest © 2009 - <?php echo date('Y'); ?>. Alguns direitos reservados.</p>
+		</footer>
+		<!-- Footer Wordpress -->
+		<?php  wp_footer(); ?>
+    </body>
+</html>
