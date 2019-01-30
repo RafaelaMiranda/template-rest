3f34ec1561ee240cc6f4b5ee84f66f0f1047b55e
Rafaela Miranda <rafaela-gomes@outlook.com>
2019-01-29 22:06:43 -0200

    Aula 4.4

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php get_header(); ?>
-	<p>Usando template Index</p>
+	<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+		<?php the_title(); ?>
+		<?php the_content(); ?>
+	<?php endwhile; else: ?>
+	<section class="container sobre">
+		<div class="grid-8">
+			<p>Essa página não existe</p>
+		</div>
+	</section>
+	<?php endif; ?>
 <?php get_footer(); ?>
